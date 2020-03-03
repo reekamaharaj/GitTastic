bd7dbf98f1ca63d3a749c946228401545cb3c926
Reeka <reekamaharaj@gmail.com>
2020-03-03 15:59:00 -0600

    started working on JS and css

--- README.md
+++ README.md
@@ -1,35 +1,10 @@
-# GitTastic
+# GifTastic
 
-   * Fool around with the GIPHY API. [Giphy API](https://developers.giphy.com/docs/).
-   * Be sure to read about these GIPHY parameters (hint, hint):
-     * `q`
-     * `limit`
-     * `rating`
-   * Make sure you switch the protocol in the query URL from **`http to https`**, or the app may not work properly when deployed to Github Pages.
-
-### Instructions
-
-2. Your app should take the topics in this array and create buttons in your HTML.
-   * Try using a loop that appends a button for each string in the array.
-
-3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-
-4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-5. Under every gif, display its rating (PG, G, so on).
-   * This data is provided by the GIPHY API.
-   * Only once you get images displaying with button presses should you move on to the next step.
-
-6. Add a form to your page that takes a value from a user input box and adds it to your `topics` array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
-
-7. Deploy your assignment to Github Pages.
+Used the GIPHY API to make a dynamic web page to populate gifs. User can add buttons and populate any gifs that want. 
 
 ### Bonus Goals
 
 1. Ensure your app is fully mobile responsive.
-
-2. Allow users to request additional gifs to be added to the page.
-   * Each request should ADD 10 gifs to the page, NOT overwrite the existing gifs.
 
 3. List additional metadata (title, tags, etc) for each gif in a clean and readable format.
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,15 @@
-let topic = ["cat", "dog", "bird"];
+let topics = ["cat", "dog", "bird"];
+        addButton();
 
+        function addButton() {
+            $("#buttons").empty();
+            for (let i = 0; i < topics.length; i++) {
+                let a = $("<button>");
+                a.addClass("gif");
+                a.attr("data-name", topics[i]);
+                a.text(topics[i]);
+                $("#buttons").append(a);
+            }
             $("button").on("click", function () {
                 let gif = $(this).attr("data-name");
                 let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -16,15 +26,95 @@ let topic = ["cat", "dog", "bird"];
                             let rating = results[i].rating;
                             let p = $("<p>").text("Rating: " + rating);
                             let gifImage = $("<img>");
-                            gifImage.attr("src", results[i].images.fixed_height.url);
+                            gifImage.attr("src", results[i].images.fixed_height_still.url);
                             gifImage.attr("data-still", results[i].images.fixed_height_still.url);
                             gifImage.attr("data-animate", results[i].images.fixed_height.url);
                             gifImage.attr("data-state", "animate");
                             gifImage.attr("alt", "gif image");
                             gifImage.addClass("gifImg");
-                            gifDiv.append(p);
                             gifDiv.append(gifImage);
+                            gifDiv.append(p);
                             $("#gifContainer").prepend(gifDiv);
                         }
+
+                        $(".gifImg").on("click", function () {
+                            let state = $(this).attr("data-state");
+                            if (state === "still") {
+                                $(this).attr("src", $(this).attr("data-animate"));
+                                $(this).attr("data-state", "animate");
+                            }
+                            else {
+                                $(this).attr("src", $(this).attr("data-still"));
+                                $(this).attr("data-state", "still");
+                            }
+                        })
                     });
             });
+        }
+
+        $("#gifsearch").on("click", function (event) {
+            event.preventDefault();
+            let search = $("#searchbox").val();
+            topics.push(search);
+            addButton();
+        })
+
+
+        /* giphy notes 
+
+        Add to API URL to get random gifs
+        e826c9fc5c929e0d6c6d423841a282aa
+
+        q: string, search query term or phrase
+        rating: string, filters by rating
+        limit: integer number of objects returned
+        
+        Gif URL
+        api.giphy.com/v1/gifs/search
+
+        Sticker URL
+        api.giphy.com/v1/stickers/
+        
+        parameters: api_key, q,limit, offset, rating, lang, random_id
+        ==================================================================
+
+        Trending GIF URL
+        api.giphy.com/v1/gifs/trending
+
+        Trending sticker URL
+        api.giphy.com/v1/stickers/trending
+
+        parameters: api_key, limit, offset, rating, random_id
+
+        ==================================================================
+
+        Translate URL gif
+        api.giphy.com/v1/gifs/translate
+
+        Translate sticker URL
+        api.giphy.com/v1/stickers/translate
+
+        parameters: api_key, s, weirdness (1-10), random_id
+
+        ==================================================================
+
+        Random URL gif
+        api.giphy.com/v1/gifs/random
+
+        Translate sticker URL
+        api.giphy.com/v1/stickers/random
+
+        parameters: api_key, tag, rating, random_id
+
+        ==================================================================
+
+        Translate URL gif
+        api.giphy.com/v1/gifs/translate
+
+        Translate sticker URL
+        api.giphy.com/v1/stickers/translate
+
+        parameters: api_key, s, weirdness (1-10), random_ids
+
+        ==================================================================
+        */
